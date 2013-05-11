3addae1e2ce75556a2fa092727d2e316642f5d68
J David Eisenberg <jdavid.eisenberg@gmail.com>
2013-05-10 18:16:10 -0700

    Delete a variety of unused files. Add a modified file.

--- code/ch06-01/stats.ex
+++ code/ch06-01/stats.ex
@@ -39,4 +39,41 @@ defmodule Stats do
     minimum(tail, head)
   end
   
+  @doc "Recursively find the maximum entry in a list of numbers."
+  @spec maximum([number]) :: number
+  
+  def maximum(list) do
+    [head | tail] = list
+    maximum(tail, head)
+  end
+  
+  # When there are no more numbers, return the result.
+  
+  @spec maximum([number], number) :: number
+  
+  defp maximum([], result) do
+    result
+  end
+  
+  # If the current result is greater than the first item in the list,
+  # keep it as the result and recursively look at the remainder of the list.
+  
+  defp maximum([head | tail], result) when result > head do
+    maximum(tail, result)
+  end
+  
+  # Otherwise, the head of the list becomes the new maximum,
+  # and recursively look at the remainder of the list.
+  
+  defp maximum([head | tail], _result) do
+    maximum(tail, head)
+  end
+  
+  # @doc "Find the range of a list of numbers as a list [min, max]."
+  @spec range([number]) :: [number]
+  
+  def range(list) do
+    [minimum(list), maximum(list)]
+  end
+
 end
